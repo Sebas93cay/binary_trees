da8e0ef493a22bfc25b9d426540a94b9cb3a5ee4
Jaime Aricapa <2241@holbertonschool.com>
2021-07-20 11:49:21 -0500

    update task 100

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,6 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling_2 - finds the sibling of a node
+ * @node: a pointer to the node to find the sibling
+ * Return: pointer to the sibling node
+ */
