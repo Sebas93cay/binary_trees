00d5757d98fb939f5cb130919ce5e45e6f39394b
Seb Asus VS <sebas93cay@gmail.com>
2021-07-21 15:38:08 -0500

    Fix 114

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -29,6 +29,31 @@ void new_offspring(bst_t *parent, bst_t *old_son, bst_t *new_son)
 }
 
 /**
+ * replace_with_node_at_right - replace node_to_remove with the
+ * lease node in the right tree
+ * @node_to_remove: node to remove;
+ * Return: node which was node_to_remove replaced for
+ */
+bst_t *replace_with_node_at_right(bst_t *node_to_remove)
+{
+	bst_t *node = NULL;
+
+	node = least_node(node_to_remove->right);
+	node->left = node_to_remove->left;
+	if (node->left)
+		node->left->parent = node;
+	if (node != node_to_remove->right)
+	{
+		node->parent->left = node->right;
+		if (node->right)
+			node->right->parent = node->parent;
+		node->right = node_to_remove->right;
+		node->right->parent = node;
+	}
+	return (node);
+}
+
+/**
  * bst_remove_recursive - remove a node recursively
  * @node: node to start looking for value in the tree
  * @value: value to remove in the tree
@@ -46,18 +71,7 @@ bst_t *bst_remove_recursive(bst_t *node, int value, bst_t *root)
 	{
 		node_to_remove = node;
 		if (node->right)
-		{
-			node = least_node(node->right);
-			node->left = node_to_remove->left;
-			if (node->left)
-				node->left->parent = node;
-			if (node != node_to_remove->right)
-			{
-				node->parent->left = node->right;
-				node->right = node_to_remove->right;
-				node->right->parent = node;
-			}
-		}
+			node = replace_with_node_at_right(node_to_remove);
 		else
 			node = node->left;
 		if (node)
